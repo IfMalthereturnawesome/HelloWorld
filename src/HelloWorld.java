dd714db46e4dc5384558f0e67ff7e40ac363ae94
IfMalthereturnawesome <malt996b@stud.kea.dk>
2021-09-28 22:07:22 +0200

    First commit ever bro

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("\"Hello\" Wordly \t \n How are you");
         System.out.print("look at \\ me");
-
+        System.out.println("Godamn noobs");
     }
 }
